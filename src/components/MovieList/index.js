ee5fb8223dc8d66566c95f26efc880bc4e2cef99
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-10-20 21:31:01 +0900

    feat: 평점 0점의 처리

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -6,10 +6,11 @@ export default function test({ movies }) {
   const render = movies.map((item) => {
     const ratingClass = item.rating >= 9 ? "ratingGood" : item.rating >= 7 ? "ratingSoso" : "ratingBad"
     const ratingIcon = item.rating >= 9 && "🔥"
+    const rating = item.rating || "평가없음"
 
     return (
       <div className='movie' key={item.id}>
-        <a className='movieTitle' href={item.url}>{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
+        <a className='movieTitle' href={item.url}>{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{rating}</span> / 10 &#41;</a>
         <div className='movieYear'>{item.year}</div>
         <img className='movieImage' src={item.background_image} alt={item.title}></img>
       </div>
