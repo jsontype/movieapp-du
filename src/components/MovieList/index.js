820a16c9cf9287dec8caca3e52a9f45b65a6f13d
azumaapp <ydhvocal@gmail.com>
2022-10-20 20:40:47 +0900

    feat: hoticon

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -5,10 +5,11 @@ export default function test({ movies }) {
   
   const render = movies.map((item) => {
     const ratingClass = item.rating >= 9 ? "ratingGood" : item.rating >= 7 ? "ratingSoso" : "ratingBad"
+    const ratingIcon = item.rating >= 9 && "🔥"
 
     return (
       <div className='movie' key={item.id}>
-        <a className='movieTitle' href={item.url}>{item.title} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
+        <a className='movieTitle' href={item.url}>{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
         <div className='movieYear'>{item.year}</div>
         <img className='movieImage' src={item.background_image} alt={item.title}></img>
       </div>
