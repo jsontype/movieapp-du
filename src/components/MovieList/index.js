34af93f8f9c977004a3574ece1e0d5c518f165a4
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-10-20 22:22:35 +0900

    feat: 컴포넌트하기 전 준비단계

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
-export default function test({ movies }) {
   
+export default function MovieList ({ movies }) {
+  const [isDetail, setIsDetail] = useState(false)
+
   const render = movies.map((item) => {
     const ratingClass = item.rating >= 9 ? "ratingGood" : item.rating >= 7 ? "ratingSoso" : "ratingBad"
     const ratingIcon = item.rating >= 9 && "🔥"
-    const rating = item.rating || "평가없음"
+    const rating = item.rating || "평가없음"    
 
+    const onClick = () => {
+      setIsDetail(!isDetail)
+    }
+
+    const renderDetail = (
+      <div>asdf</div>
+    )
+    
     return (
-      <div className='movie' key={item.id}>
-        <a className='movieTitle' href={item.url}>{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{rating}</span> / 10 &#41;</a>
-        <div className='movieYear'>{item.year}</div>
-        <img className='movieImage' src={item.background_image} alt={item.title}></img>
+      <div key={item.id}>
+        <div className='movie' onClick={onClick}>
+          <a className='movieTitle' href="#">{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{rating}</span> / 10 &#41;</a>
+          <div className='movieYear'>{item.year}</div>
+          <img className='movieImage' src={item.background_image} alt={item.title}></img>
+        </div>
+        {isDetail && renderDetail}
       </div>
     )
   })
