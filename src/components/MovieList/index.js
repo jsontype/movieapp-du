36176ec64c8dcf776831687ba7e287f76e189253
azumaapp <ydhvocal@gmail.com>
2022-10-20 20:19:43 +0900

    feat: rating

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import './style.css'
 
 export default function test({ movies }) {
+  
   const render = movies.map((item) => {
+    const ratingClass = item.rating >= 9 ? "ratingGood" : item.rating >= 7 ? "ratingSoso" : "ratingBad"
+
     return (
       <div className='movie' key={item.id}>
-        <a className='movieTitle' href={item.url}>{item.title}</a>
+        <a className='movieTitle' href={item.url}>{item.title} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
         <div className='movieYear'>{item.year}</div>
         <img className='movieImage' src={item.background_image} alt={item.title}></img>
       </div>
